20409a798ba64f19a74713942403f763fd02b319
Shubham Kanaujiya <shubhamkanaujiya505@gmail.com>
2022-07-31 22:24:53 +0530

    intial commit connect to database

---
+++ DataBase.php
@@ -0,0 +1,54 @@
+<?php
+
+// create class database name
+
+class database{
+
+    // connectivity perform
+    // database select,insert,update,delete
+
+    // make private because any body cannot access this private variable
+    private $host;
+    private $dbusername;
+    private $dbpassword;
+    private $dbname;
+
+    //this function is protected because access by only inherited class 
+    protected function connect() {
+
+        // declar the connection variable
+        $this->host='localhost';
+        $this->dbusername='$root';
+        $this->dbpassword=' ';
+        $this->dbname='crud';
+
+        // create a object for connection
+        // using connect method to using oops concept 
+        $con = new mysqli($this->host,$this->dbusername,$this->dbpassword,$this->dbname);
+
+        return $con; // because return all con value
+
+    }
+
+}
+
+// create a new class inherit database class 
+class query extends database{
+
+    // create a function 
+    public function getData(){ // this function retrive all data from the database
+
+        // write a query to the database
+        $sql = " select * from users";
+
+        // for run sql query 
+        $result = $this->connect()->query($sql);
+        
+        // for checking the value
+        print_r($result);
+
+    }
+}
+
+
+?>
